12a87a6c8b6753e5cf293213488c8a6642267ce5
Aravindrajaa <arawindrockz@gmail.com>
2022-09-09 19:58:23 +0530

    loop example

---
+++ src/org/loop/AddorEvenNumber.java
@@ -0,0 +1,17 @@
+package org.loop;
+
+public class AddorEvenNumber {
+	public static void main(String args[]) {
+		int number=11;
+		if(number%2==0) {
+			System.out.println("EVEN NUMBER");
+		}
+			else
+			{
+				System.out.println("odd number");
+			}
+				
+		}
+	}
+
+
---
+++ src/org/loop/ForLoop.java
@@ -0,0 +1,15 @@
+package org.loop;
+
+public class ForLoop {
+	public static void main(String args []) {
+		
+		for(int i=100;i>=90;i--)
+		{
+			System.out.println(i);
+		}
+		System.out.println("program exited");
+	}
+
+}
+
+
